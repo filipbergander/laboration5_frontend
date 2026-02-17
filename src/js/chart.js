3cd6d2622c56bc1960de1430cd036345ccd71fa4
Filip <filip.bergander1996@gmail.com>
2026-02-17 10:41:07 +0100

    Skapat stapeldiagram och cirkeldiagram.

--- src/js/chart.js
+++ src/js/chart.js
@@ -1,8 +1,9 @@
   "use strict";
   import '/src/sass/main.scss';
 
-  addEventListener("DOMContentLoaded", () => {
+  addEventListener("DOMContentLoaded", async() => {
       fetchStatistic();
+
   });
   /**
    * Hämtar in data från externt API.
@@ -20,4 +21,61 @@
       } catch (error) {
           console.error(error.message);
       }
+  }
+
+  /**
+   * Tar emot data från API:et för antal sökande och skapar ett stapeldiagram och cirkeldiagram utifrån datan
+   * @param {array} result 
+   */
+  function showDiagram(result) {
+
+      const filterCourse = result.filter(item => item.type === "Kurs");
+
+      const sortedData = [...filterCourse].sort((a, b) => b.applicantsTotal - a.applicantsTotal);
+
+      const topSix = sortedData.slice(0, 6);
+
+      const labels = topSix.map(item => item.name);
+
+      const applicants = topSix.map(item => item.applicantsTotal);
+
+      const ctx = document.getElementById("myChart");
+      new Chart(ctx, {
+          type: 'bar',
+          data: {
+              labels: labels,
+              datasets: [{
+                  label: 'Antal sökande på kurs',
+                  data: applicants
+              }]
+          },
+          options: { /* För att göra diagrammet responsivt och att det skalas ned */
+              responsive: true,
+              maintainAspectRatio: false
+          }
+      });
+
+      const filterProgram = result.filter(item => item.type === "Program");
+      const sortedProgram = [...filterProgram].sort((a, b) => b.applicantsTotal - a.applicantsTotal);
+
+      const topFive = sortedProgram.slice(0, 5);
+
+      const labelsCircle = topFive.map(item => item.name);
+      const applicantsCircle = topFive.map(item => item.applicantsTotal);
+      const circleDiagram = document.getElementById("circle-students");
+
+      new Chart(circleDiagram, {
+          type: 'doughnut',
+          data: {
+              labels: labelsCircle,
+              datasets: [{
+                  label: 'Antal sökande på programmet',
+                  data: applicantsCircle
+              }]
+          },
+          options: { /* För att göra diagrammet responsivt och att det skalas ned */
+              responsive: true,
+              maintainAspectRatio: false
+          }
+      });
   }
