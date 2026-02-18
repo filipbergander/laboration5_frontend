0f896d669cc34b9f86d6c295a99e3af135859247
Filip <filip.bergander1996@gmail.com>
2026-02-18 20:22:28 +0100

    Fungerande karta.

--- readme.md
+++ readme.md
@@ -1,5 +1,7 @@
 # Laboration 5 - Dynamiska webbplatser
 
-En laboration inom kursen Frontend-utveckling där man fortsätter arbetet från de tidigare laborationerna i kursen och fortsätter med att bygga diagram och en karta på hemsidan.
+En laboration inom kursen Frontend-utveckling där jag fortsätter arbetet från de tidigare laborationerna i kursen. 
+
+Inom laboration 5 skapar jag en ny sida, Diagram, med ett stapel- och cirkeldiagram utifrån antal sökande på kurs & program inom MIUN. Ytterligare en till sida skapas, karta, där jag infogar en inbäddad frame från Openstreetmap. En sökruta finns med där sökinnehållet konverteras till koordinater genom "Nominatim". Koordinaterna genererar sedan en markör för platsen på den inbäddade kartan. 
 
 Webbplats: https://lab5frontend-fb.netlify.app/
--- src/js/map.js
+++ src/js/map.js
@@ -14,19 +14,16 @@ addEventListener("DOMContentLoaded", async() => {
 
 /**
  * Söker på longitud och latitud beroende på sökinnehåll
- * @param {string} search - Sökinnehåll 
+ * @param {string} searchInput - Textfältets sökinnehåll
  */
 async function searchAdress(searchInput) {
     const url = `https://nominatim.openstreetmap.org/search?q=${searchInput}&format=json`;
     try {
         const response = await fetch(url, {
             headers: {
-                "User-Agent": "fibe.2504@student.miun.se"
+                "User-Agent": "Laboration5Frontend (fibe.2504@student.miun.se)"
             }
-        })
-        if (!response.ok) {
-            throw new Error(`Status på respons: ${response.status}`);
-        }
+        });
         const data = await response.json();
         console.log(data);
         const latitude = data[0].lat;
@@ -38,19 +35,22 @@ async function searchAdress(searchInput) {
         const maxLon = bbox[2];
         const maxLat = bbox[3];
         console.log(latitude, longitude, minLon, minLat, maxLon, maxLat);
-        showPoint(latitude, longitude, minLon, minLat, maxLon, maxLat)
+        showPoint(latitude, longitude, minLon, minLat, maxLon, maxLat);
     } catch (error) {
-        console.error(error.message);
+        console.error("Felmeddelanden: ", error);
     }
 }
 /**
- * 
- * @param {*} latitude 
- * @param {*} longitude 
+ * Genererar en markör på en inbäddad karta från OpenStreetMap beroende på sökinput och platsens longitud och latitud
+ * @param {number} latitude - Latitud för markören
+ * @param {number} longitude - Longitud för markören
+ * @param {number} minLon - Minsta longitud för platsen
+ * @param {number} minLat - Minsta latitud för platsen
+ * @param {number} maxLon - Största longitud för platsen
+ * @param {number} maxLat - Största latitud för platsen
  */
-function showPoint(latitude, longitude) {
+function showPoint(latitude, longitude, minLon, minLat, maxLon, maxLat) {
     const frameEl = document.getElementById("frame");
-    console.log(frameEl)
     frameEl.src =
-        `https://www.openstreetmap.org/export/embed.html?layer=mapnik&marker=${latitude},${longitude}&zoom=${15}`;
+        `https://www.openstreetmap.org/export/embed.html?bbox=${minLon},${minLat},${maxLon},${maxLat}&layer=mapnik&marker=${latitude},${longitude}`;
 }
