c98faf4c4857694bd3496a34eef7777cb1c21f85
Filip <filip.bergander1996@gmail.com>
2026-02-20 15:20:51 +0100

    Felsök: Tar bort docs från .gitignore för att de ska bli versionshanterade och uppladdade.

--- vite.config.js
+++ vite.config.js
@@ -16,8 +16,7 @@ export default defineConfig({
                 sass: resolve(__dirname, "sass.html"),
                 animation: resolve(__dirname, "animation.html"),
                 diagram: resolve(__dirname, "diagram.html"),
-                map: resolve(__dirname, "map.html"),
-                docs: resolve(__dirname, "public/docs/index.html")
+                map: resolve(__dirname, "map.html")
             }
         }
     },
