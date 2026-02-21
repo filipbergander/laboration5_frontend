10cff3f75e84783b6de52b7ce471cdfb2fd52c85
Filip <filip.bergander1996@gmail.com>
2026-02-21 14:55:58 +0100

    Skapade ny karta genom Leaflet. Detta gör att zoomning går att justera enklare vid sökningen med marker till skillnad mot tidigare med enbart OSM + nominatim.

---
+++ src/js/map copy.js
@@ -0,0 +1,59 @@
+"use strict";
+import '/src/sass/main.scss';
+
+
+// När DOM har laddats färdigt kan funktionen för att hämta koordinater anropas
+addEventListener("DOMContentLoaded", async() => {
+    const searchBtnEl = document.getElementById("searchBtn");
+    searchBtnEl.addEventListener("click", () => { // Om man klickar på knappen
+        const searchInput = document.getElementById("searchInput").value;
+        if (!searchInput.trim()) return;
+        searchAdress(searchInput);
+    });
+});
+
+
+/**
+ * Söker på koordinater som longitud och latitud beroende på sökinnehåll som användaren skrivit i sökfältet
+ * genom API:et Nominatim. Datan skickas sedan vidare till funktionen showPoint. 
+ * @param {string} searchInput - Sökinnehåll som användaren anger i sökfältet
+ * @async
+ */
+async function searchAdress(searchInput) {
+    const url = `https://nominatim.openstreetmap.org/search?q=${searchInput}&format=json`;
+    try {
+        const response = await fetch(url, {
+            headers: {
+                "User-Agent": "Laboration5Frontend (fibe.2504@student.miun.se)"
+            }
+        });
+        const data = await response.json();
+        console.log(data);
+        const latitude = data[0].lat;
+        const longitude = data[0].lon;
+        const bbox = data[0].boundingbox;
+
+        const minLon = bbox[0];
+        const minLat = bbox[1];
+        const maxLon = bbox[2];
+        const maxLat = bbox[3];
+        console.log(latitude, longitude, minLon, minLat, maxLon, maxLat);
+        showPoint(latitude, longitude, minLon, minLat, maxLon, maxLat);
+    } catch (error) {
+        console.error("Felmeddelanden: ", error);
+    }
+}
+/**
+ * Genererar en markör på en inbäddad karta från OpenStreetMap beroende på sökinput och platsens longitud och latitud
+ * @param {string} latitude - Latitud för markören
+ * @param {string} longitude - Longitud för markören
+ * @param {string} minLon - Minsta longitud för platsen
+ * @param {string} minLat - Minsta latitud för platsen
+ * @param {string} maxLon - Största longitud för platsen
+ * @param {string} maxLat - Största latitud för platsen
+ */
+function showPoint(latitude, longitude, minLon, minLat, maxLon, maxLat) {
+    const frameEl = document.getElementById("frame");
+    frameEl.src =
+        `https://www.openstreetmap.org/export/embed.html?bbox=${minLon},${minLat},${maxLon},${maxLat}&layer=mapnik&marker=${latitude},${longitude}`;
+}
--- src/js/map.js
+++ src/js/map.js
@@ -1,10 +1,21 @@
 "use strict";
 import '/src/sass/main.scss';
 
+let map;
+let marker;
+const searchPlaceContainer = document.querySelector(".searchPlace");
+
 
 // När DOM har laddats färdigt kan funktionen för att hämta koordinater anropas
 addEventListener("DOMContentLoaded", async() => {
+    map = L.map('map').setView([54.50, 14.68], 4);
     const searchBtnEl = document.getElementById("searchBtn");
+
+    L.tileLayer('https://tile.openstreetmap.org/{z}/{x}/{y}.png', {
+        maxZoom: 19,
+        attribution: '&copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a>'
+    }).addTo(map);
+
     searchBtnEl.addEventListener("click", () => { // Om man klickar på knappen
         const searchInput = document.getElementById("searchInput").value;
         if (!searchInput.trim()) return;
@@ -12,13 +23,6 @@ addEventListener("DOMContentLoaded", async() => {
     });
 });
 
-
-/**
- * Söker på koordinater som longitud och latitud beroende på sökinnehåll som användaren skrivit i sökfältet
- * genom API:et Nominatim. Datan skickas sedan vidare till funktionen showPoint. 
- * @param {string} searchInput - Sökinnehåll som användaren anger i sökfältet
- * @async
- */
 async function searchAdress(searchInput) {
     const url = `https://nominatim.openstreetmap.org/search?q=${searchInput}&format=json`;
     try {
@@ -31,29 +35,26 @@ async function searchAdress(searchInput) {
         console.log(data);
         const latitude = data[0].lat;
         const longitude = data[0].lon;
-        const bbox = data[0].boundingbox;
-
-        const minLon = bbox[0];
-        const minLat = bbox[1];
-        const maxLon = bbox[2];
-        const maxLat = bbox[3];
-        console.log(latitude, longitude, minLon, minLat, maxLon, maxLat);
-        showPoint(latitude, longitude, minLon, minLat, maxLon, maxLat);
+        console.log(latitude, longitude);
+        addMarker(latitude, longitude, searchInput);
     } catch (error) {
         console.error("Felmeddelanden: ", error);
     }
 }
-/**
- * Genererar en markör på en inbäddad karta från OpenStreetMap beroende på sökinput och platsens longitud och latitud
- * @param {string} latitude - Latitud för markören
- * @param {string} longitude - Longitud för markören
- * @param {string} minLon - Minsta longitud för platsen
- * @param {string} minLat - Minsta latitud för platsen
- * @param {string} maxLon - Största longitud för platsen
- * @param {string} maxLat - Största latitud för platsen
- */
-function showPoint(latitude, longitude, minLon, minLat, maxLon, maxLat) {
-    const frameEl = document.getElementById("frame");
-    frameEl.src =
-        `https://www.openstreetmap.org/export/embed.html?bbox=${minLon},${minLat},${maxLon},${maxLat}&layer=mapnik&marker=${latitude},${longitude}`;
+
+function addMarker(latitude, longitude, searchInput) {
+    if (marker) {
+        map.removeLayer(marker);
+    }
+    map.setView([latitude, longitude], 12);
+    marker = L.marker([latitude, longitude]).addTo(map);
+
+    localStorage.setItem("latitude", latitude);
+    localStorage.setItem("longitude", longitude);
+    const letterUppercase = searchInput.charAt(0).toUpperCase() + searchInput.slice(1).toLowerCase();
+    searchPlaceContainer.innerHTML =
+        `   <p> Plats du sökte på: <span id="place">${letterUppercase}</span></p>
+            <p> Latitude: <span id="lat">${latitude}</span></p>
+            <p> Longitude: <span id="lon">${longitude}</span></p>
+        `
 }
